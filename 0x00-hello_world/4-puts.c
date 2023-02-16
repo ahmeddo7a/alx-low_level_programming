55567d0844ad0f2d3fff437c6d154380530e31dc
ahmeddo7a <ahmedholdo@gmail.com>
2023-02-16 00:13:27 -0800

    fix for betty linter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/* This app prints a string using puts function*/
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's prints a string using puts function'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
