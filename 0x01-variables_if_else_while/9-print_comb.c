ff53bdca55eea6740e01461cca0a7a13f290839a
ahmeddo7a <ahmedholdo@gmail.com>
2023-02-17 18:24:53 -0800

    adding space and comma with putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program prints from a - z using putchar function '
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ascii;
+
+	ascii = 48;
+
+	while (ascii < 58)
+	{
+		putchar(ascii);
+		if (ascii != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		ascii++;
+	}
+	return (0);
+}
