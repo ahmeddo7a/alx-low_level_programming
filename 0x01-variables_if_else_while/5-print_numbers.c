50fa3e24eed04ee6cc364d273f6af554ba45aa2e
ahmeddo7a <ahmedholdo@gmail.com>
2023-02-17 17:29:53 -0800

    Print Numbers from 0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program prints from a - z using putchar function '
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
