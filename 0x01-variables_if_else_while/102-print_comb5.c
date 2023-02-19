9c8831dcda9c4d3b006b894eea436a1aa92ef979
ahmeddo7a <ahmedholdo@gmail.com>
2023-02-19 15:28:29 -0800

    The Hardest algorithm untill now

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program prints digits which are different '
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if(i < j)
+			{
+				putchar((i/10) + 48);
+				putchar((i%10) + 48);
+				putchar(32);
+				putchar((j/10) + 48);
+				putchar((j%10) + 48);
+				if(i != 98 || j!= 99)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}				
