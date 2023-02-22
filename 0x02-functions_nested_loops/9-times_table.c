857b5557eb8ea1744b5fe25b662f0ad60dfdb487
ahmeddo7a <ahmedholdo@gmail.com>
2023-02-21 17:10:17 -0800

    times table fix output the last comma in a row error task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,24 +18,24 @@ void times_table(void)
 	{
 		for (y = 0; y < 10; y++)
 		{
-			if ((x * y) > 9)
+			z = x * y;
+			if (y == 0)
 			{
-				z = x * y;
+				_putchar(z + 48);
+			}
+			if (z < 10 && y != 0)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				_putchar(z + 48);
+			}
+			else if (z >= 10)
+			{
+				_putchar(44);
+				_putchar(32);
 				_putchar((z / 10) + 48);
 				_putchar((z % 10) + 48);
-				_putchar(44);
-				_putchar(32);
-			}
-			else
-			{
-				_putchar((x * y) + 48);
-				_putchar(44);
-				_putchar(32);
-				if ((x * (y + 1)) < 10)
-				{
-					_putchar(32);
-				}
-
 			}
 		}
 		putchar('\n');
