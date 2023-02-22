a4e5705e097c0eb41536f60c424a97bed4a15cdf
ahmeddo7a <ahmedholdo@gmail.com>
2023-02-21 17:18:23 -0800

    Adding two integer numbers task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add two numbers
+ * @x: first number
+ * @y: second number
+ * Return: addition result
+ */
+
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+
+	return (result);
+}
