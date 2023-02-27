ecfc2c866e35c2e778ecb822b7a4a9e8d3b87db5
ahmeddo7a <ahmedholdo@gmail.com>
2023-02-27 13:17:49 -0800

    New project directory and first task to reset value to 98 is done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset number to 98x
+ * @n: The pointer which point to the needed variable to reset
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	int m = 98;
+
+	*n = m;
+}
