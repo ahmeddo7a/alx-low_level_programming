862b685fe1b1a3ba8309409c294e95dcf717907d
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-02 10:17:30 -0800

    fix task 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strncpy - writes the character c to stdout
+ * _strncat - writes the character c to stdout
  * @dest: The String where the src will be added
  * @src: the String where it will be added to dest
  * @n: the number of bytes
@@ -11,11 +11,12 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int i;
+	int i;
 	int j;
 
-        i = 0;
+	i = 0;
 	j = 0;
+
 	while (dest[i] != '\0')
 	{
 		i++;
