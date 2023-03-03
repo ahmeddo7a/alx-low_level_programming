dcd96bdd6b8942ff20a3ed5b59d78cd541b6e0b6
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-02 18:11:26 -0800

    rot13 encryption

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,24 +8,22 @@
 
 char *rot13(char *c)
 {
-        char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-        int i;
-        int j;
+	int i;
+	int j;
 
-
-        for (i = 0; c[i] != '\0'; i++)
-        {
-                for (j = 0; j < 52; j++)
-                {
-                        if (c[i] == s1[j])
-                        {
-                                c[i] = s2[j];
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (c[i] == s1[j])
+			{
+				c[i] = s2[j];
 				break;
-                        }
-                }
-        }
-        return (c);
+			}
+		}
+	}
+	return (c);
 }
-
