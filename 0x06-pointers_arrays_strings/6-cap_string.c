870f846673e989c0055b9236af6dbe65f9e312f3
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-02 17:32:27 -0800

    capitalize after the pattern task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize First letter after any of the pattern
+ * @c: String array
+ *
+ * Return: Char , String after capitalize
+ */
+
+char *cap_string(char *c)
+{
+	int i;
+
+	i = 0;
+
+	while (c[i] != '\0')
+	{
+		while (!(c[i] >= 97 && c[i] <= 122))
+		{
+			i++;
+		}
+		if (c[i - 1] == ' ' ||
+				c[i - 1] == '\t' ||
+				c[i - 1] == '\n' ||
+				c[i - 1] == ',' ||
+				c[i - 1] == ';' ||
+				c[i - 1] == '.' ||
+				c[i - 1] == '!' ||
+				c[i - 1] == '?' ||
+				c[i - 1] == '"' ||
+				c[i - 1] == '(' ||
+				c[i - 1] == ')' ||
+				c[i - 1] == '{' ||
+				c[i - 1] == '}')
+		{
+			c[i] = c[i] - 32;
+		}
+		i++;
+	}
+	return (c);
+}
