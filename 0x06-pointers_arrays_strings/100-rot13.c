53ada796f088b92a4c10ede5fa975413a64e2214
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-02 18:08:40 -0800

    rot13 encryption

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * rot13 - encryption using rot 13
+ * @c: it the string to encode
+ *
+ * Return: the value of c
+ */
+
+char *rot13(char *c)
+{
+        char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+        int i;
+        int j;
+
+
+        for (i = 0; c[i] != '\0'; i++)
+        {
+                for (j = 0; j < 52; j++)
+                {
+                        if (c[i] == s1[j])
+                        {
+                                c[i] = s2[j];
+				break;
+                        }
+                }
+        }
+        return (c);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *x);
 char *cap_string(char *c);
 char *leet(char *c);
+char *rot13(char *);
+
 #endif
