ec41a404985528fa66dff0c812f47480db0cf987
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-16 16:38:36 -0700

    calloc task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenate two strings
+ * @s1: first string
+ * @s2: second string 
+ * @n: number of bytes
+ *
+ * Return: succes return the conc string If fails return Null
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,5 +2,7 @@
 #define Main_h
 
 void *malloc_checked(unsigned int b);
-
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *_memset(char *s, char b, unsigned int n);
 #endif
