89be952dc394d9287a58965eeb25169389882b71
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-16 19:39:38 -0700

    fix task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
 /**
  * string_nconcat - concatenate two strings
  * @s1: first string
- * @s2: second string 
+ * @s2: second string
  * @n: number of bytes
  *
  * Return: succes return the conc string If fails return Null
@@ -23,16 +23,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = 0;
 	len2 = 0;
 
-	while (s1 && s[len1])
+	while (s1 && s1[len1])
 		len1++;
-	while (s2 && s2[len])
+	while (s2 && s2[len2])
 		len2++;
 
 	if (n < len2)
 		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		s = malloc(sizeof(char) * (len1 +len2 +1));
-	if(!s)
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
 		return (NULL);
 
 	while (n < len2 && i < (len1 + n))
