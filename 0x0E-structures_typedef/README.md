0bf362bcafe587084c98dfbb1b8a709cb4b48d3b
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-21 05:44:51 -0700

    first task

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+#THis readme is for learning struct
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Dog info
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
+ *
+ * Description: Dog INFo
+ */
+
+struct dog
+{
+	char *dog;
+	char *owner;
+	float age;
+}
