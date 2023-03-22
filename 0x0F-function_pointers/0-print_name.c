a6f2ea6d53eda2c7b3c7b8e7db1e412b1533c444
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-22 04:24:41 -0700

    first task fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,8 @@
-#include "main.h"
+#include "function_pointers.h"
 #include <stdio.h>
 
-/** print_name - print name using pointer to function
+/**
+ * print_name - print name using pointer to function
  * @name: string to add
  * @f: pointer to function
  *
