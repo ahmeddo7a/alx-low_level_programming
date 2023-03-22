a7445a4de0dfd5aab9775226b0fb26de6236af62
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-22 05:00:59 -0700

    third task fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,6 +6,8 @@
  * @array: array to be entered
  * @size: size of the array
  * @cmp: pointer to func
+ *
+ * Return: the index of array if success and -1 if failed
  */
 
 int int_index(int *array, int size, int(*cmp)(int))
