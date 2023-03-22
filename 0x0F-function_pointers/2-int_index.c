8b176381440e99ba7fefe1732a6a83c6fd8f480d
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-22 04:58:58 -0700

    third task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - search for integer
+ * @array: array to be entered
+ * @size: size of the array
+ * @cmp: pointer to func
+ */
+
+int int_index(int *array, int size, int(*cmp)(int))
+{
+	int x;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (x = 0; x < size; x++)
+	{
+		if (cmp(array[x]))
+			return (x);
+	}
+	return (-1);
+}
