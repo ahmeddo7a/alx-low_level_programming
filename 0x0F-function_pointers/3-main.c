67666e564d383b7a0f65dc538d7214b29076aa7d
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-22 06:27:30 -0700

    fix calculator task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,7 +18,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("Error not equal 4\n");
+		printf("Error\n");
 		exit(98);
 	}
 	num1 = atoi(argv[1]);
@@ -27,10 +27,15 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
+		printf("Error\n");
 		exit(99);
+
 	}
 	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
 		exit(100);
+	}
 	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
