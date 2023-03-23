84707c226667bd0ddcf0aabc51495b973d91d12d
ahmeddo7a <ahmedholdo@gmail.com>
2023-03-23 13:08:36 -0700

    Print all numbers task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include "vriadic_functions.h"
+
+/**
+ * print_numbers - print numbers of all parameters inserted followed by ,
+ * @separator: string to be printed between numbers
+ * @n: number of integers to be printed
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list param_list;
+
+	unsigned int x;
+
+	va_start(param_list,n);
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", va_arg(param_list, int));
+
+		if (x != (n-1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(param_list);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,5 @@
 #ifndef variadicfunctions_h
 #define variadicfunctions_h
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
