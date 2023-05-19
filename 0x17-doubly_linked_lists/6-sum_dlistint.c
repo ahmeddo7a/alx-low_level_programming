e50f07003acc55d648629854eabe11b424de5d6a
ahmeddo7a <ahmedholdo@gmail.com>
2023-05-18 19:32:37 -0700

    Sum of all nodes task

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of nodes of a linked list.
+ * @head: pointer to the list.
+ * Return: sum of all data in the list, 0 if the lost is empty.
+ **/
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *node = head;
+	int sum = 0;
+
+	while (node)
+	{
+		sum += node->n;
+		node = node->next;
+	}
+
+	return (sum);
+}
